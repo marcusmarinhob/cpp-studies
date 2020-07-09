dea63cd914f4d7f93f26a5b103308b37feb51c00
marcusmarinhob <marcus.bezerra@ee.ufcg.edu.br>
2020-07-08 21:36:55 -0300

    Instanciando Automoveis na aplicação

--- basics/veiculos/main.cpp
+++ basics/veiculos/main.cpp
@@ -1,8 +1,11 @@
-#include <iostream>
+#include "Automovel.h"
 
 using namespace std;
 
 int main() {
+    Automovel a1;
+
+    cout << "Quantidade de rodas" << a1.getRodas() << endl;
     
     return 0;
 }
